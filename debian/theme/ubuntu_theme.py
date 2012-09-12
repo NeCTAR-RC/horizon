81c395026276faa07b2f183762a15691d39fb418
Adam Gandelman <adamg@canonical.com>
2012-09-11 22:48:09 -0700

    Only maintain templates out-of-tree.

--- debian/theme/ubuntu_theme.py
+++ debian/theme/ubuntu_theme.py
@@ -2,6 +2,5 @@
 # /usr/share/openstack-dashboard/openstack_dashboard/local/ will
 # enable the Ubuntu theme for Horizon.  To disable, remove the
 # openstack-dashboard-ubuntu-theme package, or remove this file.
-STATIC_ROOT = '/usr/share/openstack-dashboard-ubuntu-theme/static'
 TEMPLATE_DIRS = ('/usr/share/openstack-dashboard-ubuntu-theme/templates', )
 
