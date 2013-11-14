8c924b5912f6a20c4c32078dbc7074a206467e15
Kieran Spear <kispear@gmail.com>
2013-11-14 17:39:20 +1100

    Default to image boot source in launch instance
    Change-Id: I2ca213a4cfbd015de62b7ee05d235e52dbe8dffc

--- openstack_dashboard/dashboards/project/instances/workflows/create_instance.py
+++ openstack_dashboard/dashboards/project/instances/workflows/create_instance.py
@@ -86,6 +86,7 @@ class SetInstanceDetailsAction(workflows.Action):
                                help_text=_("Number of instances to launch."))
 
     source_type = forms.ChoiceField(label=_("Instance Boot Source"),
+                                    initial='image_id',
                                     help_text=_("Choose Your Boot Source "
                                                 "Type."))
 
