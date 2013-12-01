02131aa62612be9a3e3032da0923ad45c96f8b26
Kieran Spear <kispear@gmail.com>
2013-12-02 09:55:56 +1100

    Disable boot from volume (from image) for now
    Change-Id: I9781b8a9afd52761123ea3f08bc337992427b3a2

--- openstack_dashboard/dashboards/project/instances/workflows/create_instance.py
+++ openstack_dashboard/dashboards/project/instances/workflows/create_instance.py
@@ -138,14 +138,14 @@ class SetInstanceDetailsAction(workflows.Action):
             ("volume_id", _("Boot from volume.")),
         ]
 
-        try:
-            if api.nova.extension_supported("BlockDeviceMappingV2Boot",
-                                            request):
-                source_type_choices.append(("volume_image_id",
-                        _("Boot from image (creates a new volume).")))
-        except Exception:
-            exceptions.handle(request, _('Unable to retrieve extensions '
-                                         'information.'))
+        # try:
+        #     if api.nova.extension_supported("BlockDeviceMappingV2Boot",
+        #                                     request):
+        #         source_type_choices.append(("volume_image_id",
+        #                 _("Boot from image (creates a new volume).")))
+        # except Exception:
+        #     exceptions.handle(request, _('Unable to retrieve extensions '
+        #                                  'information.'))
 
         source_type_choices.append(("volume_snapshot_id",
                 _("Boot from volume snapshot (creates a new volume).")))
