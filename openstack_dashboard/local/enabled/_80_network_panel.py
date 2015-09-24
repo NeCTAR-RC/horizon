3bde636bbf4c73c6b2ace63849e755fd927f1bcd
Sam Morrison <sorrison@gmail.com>
2015-09-24 13:11:20 +1000

    Changes to not display neutron panels
    Change-Id: I9de5085cae7c9485d977efc1b40e9bb7ffba211f

--- openstack_dashboard/local/enabled/_80_network_panel.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-if getattr(settings, 'NEUTRON_DISABLED', False):
-    PANEL = 'networks'
-    PANEL_DASHBOARD = 'project'
-    PANEL_GROUP = 'project'
-    REMOVE_PANEL = True
