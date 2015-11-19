21b9bf856d970ef551d4d4e8b4b5016ab9e8632c
Sam Morrison <sorrison@gmail.com>
2015-11-20 10:58:31 +1100

    Disable multi region
    Change-Id: I0c5e63a5b413a9425de1dab1533e95e41cf66e06

--- openstack_dashboard/templatetags/context_selection.py
+++ openstack_dashboard/templatetags/context_selection.py
@@ -20,7 +20,7 @@ register = template.Library()
 
 
 def is_multi_region_configured(request):
-    return len(request.user.available_services_regions) > 1
+    return False
 
 
 # TODO(e0ne): pass OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT to the template
