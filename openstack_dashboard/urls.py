8770873b69b969954374246baec1570ece07d3bb
Sam Morrison <sorrison@gmail.com>
2015-11-26 12:03:41 +1100

    Optionally allow django-restframework at /rest_api
    Set REST_VIEW_SETS to use containing a list of tuples
    ('name', 'path.to.module.Class')
    
    Change-Id: Ie0c8d0965aaacc8dd6b385dfafd061ba154ebd87

--- openstack_dashboard/urls.py
+++ openstack_dashboard/urls.py
@@ -26,6 +26,9 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views import defaults
+from django.views.generic.base import TemplateView
+
+from oslo_utils import importutils
 
 import horizon
 
@@ -52,3 +55,16 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.append(url(r'^500/$', defaults.server_error))
+
+
+if getattr(settings, 'REST_VIEW_SETS', ()):
+    try:
+        from rest_framework import routers
+        router = routers.DefaultRouter()
+        for name, class_str in settings.REST_VIEW_SETS:
+            klass = importutils.import_class(class_str)
+            router.register(name, klass)
+
+        urlpatterns.append(url(r'^rest_api/', include(router.urls)))
+    except ImportError:
+        pass
