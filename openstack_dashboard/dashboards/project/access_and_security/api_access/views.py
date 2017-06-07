f5daa7a36ecf7fdd43cd9fd2d6e77ac841f686d8
Andy Botting <andy@andybotting.com>
2017-06-07 10:41:54 +1000

    Add API access URL to openrc.sh template
    This commit adds an echo statement to remind the user they need to
    reset their password first before trying to use the API and prints
    the the documentation URL
    
    Change-Id: Ief73f16f24954a5fe50b5b879e540a0b11db0305

--- openstack_dashboard/dashboards/project/access_and_security/api_access/views.py
+++ openstack_dashboard/dashboards/project/access_and_security/api_access/views.py
@@ -17,6 +17,7 @@ import logging
 import tempfile
 import zipfile
 
+from django.conf import settings
 from django import http
 from django import shortcuts
 from django.template.loader import render_to_string
@@ -116,6 +117,12 @@ def download_rc_file(request):
     template = 'project/access_and_security/api_access/openrc.sh.template'
     try:
         context = _get_openrc_credentials(request)
+
+        api_access_doc = ('https://support.ehelp.edu.au/'
+                          'support/solutions/articles/6000078065-api')
+        context['api_access_doc'] = getattr(settings,
+                                            'NECTAR_API_ACCESS_DOC',
+                                            api_access_doc)
 
         response = shortcuts.render(request,
                                     template,
