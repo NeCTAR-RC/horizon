420c6e273f7f02776b0b14e73cd2d9a64f690f90
Sam Morrison <sorrison@gmail.com>
2017-11-20 10:54:15 +1100

    Remove nova-cert ec2 creds
    Change-Id: Ie94467fa5ce245038efee2c5d222840259422edf

--- openstack_dashboard/dashboards/project/access_and_security/api_access/tests.py
+++ openstack_dashboard/dashboards/project/access_and_security/api_access/tests.py
@@ -33,15 +33,10 @@ class APIAccessTests(test.TestCase):
         cert = self.certs.first()
 
         self.mox.StubOutWithMock(api.keystone, "list_ec2_credentials")
-        self.mox.StubOutWithMock(api.nova, "get_x509_credentials")
-        self.mox.StubOutWithMock(api.nova, "get_x509_root_certificate")
         self.mox.StubOutWithMock(api.keystone, "create_ec2_credentials")
 
         api.keystone.list_ec2_credentials(IsA(HttpRequest), self.user.id) \
                     .AndReturn([])
-        api.nova.get_x509_credentials(IsA(HttpRequest)).AndReturn(cert)
-        api.nova.get_x509_root_certificate(IsA(HttpRequest)) \
-                .AndReturn(cert)
         api.keystone.create_ec2_credentials(IsA(HttpRequest),
                                             self.user.id,
                                             self.tenant.id).AndReturn(creds)
