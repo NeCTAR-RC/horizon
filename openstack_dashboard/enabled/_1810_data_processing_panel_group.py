d0964529cdbae7b67ff6053924fb5bd7ebabeea4
Sam Morrison <sorrison@gmail.com>
2018-01-04 11:28:36 +1100

    Add beta tag to data processing nav
    Change-Id: Ie0d55cbf4463b8a7f5ae0e613806a0be70ac8891

--- openstack_dashboard/enabled/_1810_data_processing_panel_group.py
+++ openstack_dashboard/enabled/_1810_data_processing_panel_group.py
@@ -3,6 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 # The slug of the panel group to be added to HORIZON_CONFIG. Required.
 PANEL_GROUP = 'data_processing'
 # The display name of the PANEL_GROUP. Required.
-PANEL_GROUP_NAME = _('Data Processing')
+PANEL_GROUP_NAME = _('Data Processing (BETA)')
 # The slug of the dashboard the PANEL_GROUP associated with. Required.
 PANEL_GROUP_DASHBOARD = 'project'
