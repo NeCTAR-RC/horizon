NeCTAR-RC/horizon

commit fdb9863a922f27a8806f8b278732d3c69bdbe136

Necterism: Filter networks by tag for floating networks
There is no way to tell if an external network is for floating ips
or classic provider network. Usually you can use the network for
either case if right combination of shared/external. This doesn't
work in nectar because provider networks are linuxbridge and
floating are midonet/ovn. The api doesn't provide this information
to us so we specifically tag networks that should be used as floating
ip networks.

To activate set NECTAR_FLOATING_NETWORK_TAG

Change-Id: I158f311944f4dd53e25b32f31dcb270b49d5410f

openstack_dashboard/dashboards/project/routers/ports/forms.py
@@ -14,6 +14,7 @@
 
 import logging
 
+from django.conf import settings
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -47,7 +48,11 @@ class AddInterface(forms.SelfHandlingForm):
         router_id = self.initial['router_id']
 
         try:
-            networks = api.neutron.network_list_for_tenant(request, tenant_id)
+            search_opts = {}
+            if settings.NECTAR_FLOATING_NETWORK_TAG:
+                search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
+            networks = api.neutron.network_list_for_tenant(
+                request, tenant_id, **search_opts)
             if router_id:
                 ports = api.neutron.port_list(request, device_id=router_id)
                 router_subnet_ids = [fixed_ip["subnet_id"] for port in ports
@@ -166,6 +171,9 @@ class SetGatewayForm(forms.SelfHandlingForm):
 
     def populate_network_id_choices(self, request):
         search_opts = {'router:external': True}
+        if settings.NECTAR_FLOATING_NETWORK_TAG:
+            search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
+
         try:
             networks = api.neutron.network_list(request, **search_opts)
         except Exception as e:

openstack_dashboard/dashboards/project/routers/forms.py
@@ -18,6 +18,7 @@ Views for managing Neutron Routers.
 """
 import logging
 
+from django.conf import settings
 from django.urls import reverse
 from django.urls import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
@@ -103,6 +104,8 @@ class CreateForm(forms.SelfHandlingForm):
 
     def _get_network_list(self, request):
         search_opts = {'router:external': True}
+        if settings.NECTAR_FLOATING_NETWORK_TAG:
+            search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
         try:
             networks = api.neutron.network_list(request, **search_opts)
         except Exception as e:

openstack_dashboard/api/neutron.py
@@ -556,6 +556,9 @@ class FloatingIpManager(object):
         :returns: List of FloatingIpPool objects
         """
         search_opts = {'router:external': True}
+        if settings.NECTAR_FLOATING_NETWORK_TAG:
+            search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
+
         return [FloatingIpPool(pool) for pool
                 in self.client.list_networks(**search_opts).get('networks')]
 
@@ -692,7 +695,11 @@ class FloatingIpManager(object):
                                 if p.device_id in gw_routers)
         # we have to include any shared subnets as well because we may not
         # have permission to see the router interface to infer connectivity
-        shared = set(s.id for n in network_list(self.request, shared=True)
+        search_opts = {'shared': True}
+        if settings.NECTAR_FLOATING_NETWORK_TAG:
+            search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
+
+        shared = set(s.id for n in network_list(self.request, **search_opts)
                      for s in n.subnets)
         return reachable_subnets | shared
 
@@ -1075,6 +1082,11 @@ def network_list_for_tenant(request, tenant_id, include_external=False,
         networks += network_list(request, tenant_id=tenant_id,
                                  shared=False, **params)
 
+    if settings.NECTAR_FLOATING_NETWORK_TAG:
+        params['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
+        networks = network_list(
+            request, id='00000000-0000-0000-0000-000000000000') + networks
+
     if shared in (None, True):
         # In the current Neutron API, there is no way to retrieve
         # both owner networks and public networks in a single API call.
@@ -1165,6 +1177,8 @@ def network_delete(request, network_id):
 @memoized
 def subnet_list(request, **params):
     LOG.debug("subnet_list(): params=%s", params)
+    if settings.NECTAR_FLOATING_NETWORK_TAG and 'tenant_id' not in params:
+        params['tenant_id'] = request.user.tenant_id
     subnets = neutronclient(request).list_subnets(**params).get('subnets')
     return [Subnet(s) for s in subnets]
 

openstack_dashboard/settings.py
@@ -219,6 +219,8 @@ CSRF_COOKIE_AGE = None
 
 COMPRESS_OFFLINE_CONTEXT = 'horizon.themes.offline_context'
 
+NECTAR_FLOATING_NETWORK_TAG = None
+
 # Notice all customizable configurations should be above this line
 XSTATIC_MODULES = settings_utils.BASE_XSTATIC_MODULES
 

openstack_dashboard/test/settings.py
@@ -273,3 +273,5 @@ TEST_GLOBAL_MOCKS_ON_PANELS = {
         'return_value': True,
     },
 }
+
+NECTAR_FLOATING_NETWORK_TAG = None

commit 0e919b2c7da536dbe4e426f8277d062b7a861cc9

Don't filter subnets when populating router subnet form.
fdb9863a922f27a8806f8b278732d3c69bdbe136 introduced a bug by
filtering subnets for a router subnet addition. This isn't needed
as we already filter by tenant_id and it isn't expected that users
would own networks that aren't able to hook into L3 services.

Change-Id: Ib09675d52fb298e824737ea13cb1cebbe87a30b6

openstack_dashboard/dashboards/project/routers/ports/forms.py
@@ -49,8 +49,6 @@ class AddInterface(forms.SelfHandlingForm):
 
         try:
             search_opts = {}
-            if settings.NECTAR_FLOATING_NETWORK_TAG:
-                search_opts['tags-any'] = settings.NECTAR_FLOATING_NETWORK_TAG
             networks = api.neutron.network_list_for_tenant(
                 request, tenant_id, **search_opts)
             if router_id: