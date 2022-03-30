NeCTAR-RC/horizon

commit b17eddd72e163395dd12d4d779fdcbbc3cfcb91f

When looking up image name, also look in community images
In the Instances table, the request to Glance doesn't search in
community images, so we need to explicitly search them too

Change-Id: Iff56bf4f689303c2be1a096d19440b1f7762953b

openstack_dashboard/dashboards/project/instances/views.py
@@ -23,6 +23,7 @@ from collections import OrderedDict
 import logging
 
 from django.conf import settings
+from django.core.cache import cache
 from django import http
 from django import shortcuts
 from django.urls import reverse
@@ -59,6 +60,9 @@ from openstack_dashboard.views import get_url_with_pagination
 
 LOG = logging.getLogger(__name__)
 
+CACHE_COMMUNITY_IMAGE_LIST_TIMEOUT = getattr(
+    settings, 'CACHE_COMMUNITY_IMAGE_LIST_TIMEOUT', 3600)
+
 
 class IndexView(tables.PagedTableMixin, tables.DataTableView):
     table_class = project_tables.InstancesTable
@@ -87,17 +91,20 @@ class IndexView(tables.PagedTableMixin, tables.DataTableView):
             # API call and the Glance API currently does not support filtering
             # by multiple values in the visibility field.
             # TODO(gabriel): Handle pagination.
-            images = api.glance.image_list_detailed(self.request)[0]
-            community_images = api.glance.image_list_detailed(
-                self.request, filters={'visibility': 'community'})[0]
-            image_map = {
-                image.id: image for image in images
-            }
-            # Images have to be filtered by their uuids; some users
-            # have default access to certain community images.
-            for image in community_images:
-                image_map.setdefault(image.id, image)
-            return image_map
+            image_list = api.glance.image_list_detailed(self.request)[0]
+            images = dict((str(image.id), image.name) for image in image_list)
+
+            # Use cache for community image listing
+            community_images = cache.get('community_images')
+            if community_images is None:
+                community_images = {}
+                for image in api.glance.image_list_detailed(
+                        self.request, filters={'visibility': 'community'})[0]:
+                    community_images[str(image.id)] = image.name
+                cache.add('community_images', community_images,
+                          CACHE_COMMUNITY_IMAGE_LIST_TIMEOUT)
+            images.update(community_images)
+            return images
         except Exception:
             exceptions.handle(self.request, ignore=True)
             return {}
@@ -188,7 +195,7 @@ class IndexView(tables.PagedTableMixin, tables.DataTableView):
         if isinstance(instance.image, dict):
             image_id = instance.image.get('id')
             if image_id in image_dict:
-                instance.image = image_dict[image_id]
+                instance.image['name'] = image_dict[image_id]
             # In case image not found in image_dict, set name to empty
             # to avoid fallback API call to Glance in api/nova.py
             # until the call is deprecated in api itself
@@ -217,12 +224,13 @@ class IndexView(tables.PagedTableMixin, tables.DataTableView):
             volume_metadata = getattr(boot_volume, "volume_image_metadata", {})
             image_id = volume_metadata.get('image_id')
             if image_id:
-                try:
-                    instance.image = image_dict[image_id]
-                except KeyError:
-                    # KeyError occurs when volume was created from image and
-                    # then this image is deleted.
-                    pass
+                # If the image is hidden, it won't be found in the
+                # image dict, so leave name out to resolve it with
+                # the fallback API call to Glance in api/nova.py
+                image_data = {'id': image_id}
+                if image_id in image_dict:
+                    image_data['name'] = image_dict[image_id]
+                instance.image = image_data
 
 
 def process_non_api_filters(search_opts, non_api_filter_info):

openstack_dashboard/dashboards/project/instances/tests.py
@@ -23,6 +23,7 @@ import sys
 from unittest import mock
 
 from django.conf import settings
+from django.core.cache import cache
 from django.forms import widgets
 from django import http
 import django.test
@@ -90,6 +91,7 @@ class InstanceTestBase(helpers.ResetImageAPIVersionMixin,
         else:
             self.versioned_images = self.imagesV2
             self.versioned_snapshots = self.snapshotsV2
+        cache.clear()
 
 
 class InstanceTableTestMixin(object):
@@ -441,7 +443,7 @@ class InstanceTableTests(InstanceTestBase, InstanceTableTestMixin):
         }
         servers = self._test_index_with_instance_booted_from_volume(
             image_metadata, expected_image_name=base_image.name)
-        self.assertEqual(base_image.name, servers[0].image.name)
+        self.assertEqual(base_image.name, servers[0].image_name)
 
     def test_index_with_instance_booted_from_volume_no_image_info(self):
         # Borrowed from bug #1834747

commit 85c4d08a16c6c4ef0076317a57d47d2f16fd385f

Fix up filtering instances by image name.
Commit b17eddd72e163395dd12d4d779fdcbbc3cfcb91f broke this
due to changing the format of the images. This was because we
can't cache(pickle) objects so we changed the structure to be a
dict of {'id': 'name'} as opposed to {'id': <image>}

This change is pretty hacky and we should look at refactoring this
and the mentioned commit to make this better.

Change-Id: I780268210641d713e7252c7251f2675af02152a1

openstack_dashboard/dashboards/project/instances/views.py
@@ -164,7 +164,7 @@ class IndexView(tables.PagedTableMixin, tables.DataTableView):
             )
 
         non_api_filter_info = (
-            ('image_name', 'image', image_dict.values()),
+            ('image_name', 'image', image_dict),
             ('flavor_name', 'flavor', flavor_dict.values()),
         )
         if not process_non_api_filters(search_opts, non_api_filter_info):
@@ -250,11 +250,20 @@ def _swap_filter(resources, search_opts, fake_field, real_field):
     if fake_field not in search_opts:
         return True
     filter_string = search_opts[fake_field]
-    matched = [resource for resource in resources
-               if resource.name.lower() == filter_string.lower()]
+    try:
+        matched = [resource for resource in resources
+                   if resource.name.lower() == filter_string.lower()]
+    except AttributeError:
+        # We need this as image_dict is in a different format due to caching
+        # We can't cache the objects as not pickleable so it's a dict
+        matched = [id for id, name in resources.items()
+                   if name.lower() == filter_string.lower()]
     if not matched:
         return False
-    search_opts[real_field] = matched[0].id
+    try:
+        search_opts[real_field] = matched[0].id
+    except AttributeError:
+        search_opts[real_field] = matched[0]
     del search_opts[fake_field]
     return True
 