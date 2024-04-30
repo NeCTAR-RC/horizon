5e738a108056d300f83aa22224529d75a4d59b60
Sam Morrison <sorrison@gmail.com>
2024-04-30 14:21:40 +1000

    Add tz context processor
    Change-Id: I6457e4fb4cce31479b6ebda31bb646fc8fc02d21

--- openstack_dashboard/settings.py
+++ openstack_dashboard/settings.py
@@ -111,6 +111,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
+                'django.template.context_processors.tz',
                 'django.template.context_processors.request',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
